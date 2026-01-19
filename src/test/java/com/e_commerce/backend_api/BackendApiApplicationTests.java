75bdcd0e076651004ce52e6b6df466f6ba7c6404
Vimal290704 <vimalyadavkr001@gmail.com>
2026-01-19 16:28:38 +0530

    Initializing Ecommerce Backend

---
+++ src/test/java/com/e_commerce/backend_api/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
